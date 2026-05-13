b374a7bb0fa09d8750f60c183a0facf0f3c82d9b
zktm9903 <zktm9903@naver.com>
2026-05-13 22:45:14 +0900

    feat: 동물 이미지 supabase storage 사용

--- hooks/use-selected-animals.ts
+++ hooks/use-selected-animals.ts
@@ -1,8 +1,8 @@
 export const ANIMALS = [
-  { key: "cat", src: "/1-no-image-cat.gif", label: "고양이" },
-  { key: "dog", src: "/1-no-image-dog.gif", label: "개" },
-  { key: "guinea-pig", src: "/1-no-image-guinea-pig.gif", label: "기니피그" },
-  { key: "nakta", src: "/1-no-image-nakta.gif", label: "낙타" },
+  { key: "cat", src: "https://zwavfmnwtqohwkdylpmm.supabase.co/storage/v1/object/public/animal/1-no-image-cat.webp", label: "고양이" },
+  { key: "dog", src: "https://zwavfmnwtqohwkdylpmm.supabase.co/storage/v1/object/public/animal/1-no-image-dog.webp", label: "개" },
+  { key: "guinea-pig", src: "https://zwavfmnwtqohwkdylpmm.supabase.co/storage/v1/object/public/animal/1-no-image-guinea-pig.webp", label: "기니피그" },
+  { key: "nakta", src: "https://zwavfmnwtqohwkdylpmm.supabase.co/storage/v1/object/public/animal/1-no-image-nakta.webp", label: "낙타" },
 ] as const;
 
 export { useSelectedAnimalsContext as useSelectedAnimals } from "@/components/providers/SelectedAnimalsProvider";
--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'images.steamusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'zwavfmnwtqohwkdylpmm.supabase.co',
+      },
     ],
   },
 };
